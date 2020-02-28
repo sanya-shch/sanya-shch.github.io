0f2bd7693dc2c8b5810d0da5ca4a886aaef6bcbd
Oleksandr Shchipkov <37896474+sanya-shch@users.noreply.github.com>
2020-02-28 17:03:25 +0200

    Add files via upload

---
+++ precache-manifest.3500b4b96fb41c1146b501efc0d45032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7747c9416245609bad85f20a60e7700f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c33df03f2d9b8917199d",
+    "url": "/static/css/main.ada10d47.chunk.css"
+  },
+  {
+    "revision": "14c0c76e098d5d44a81e",
+    "url": "/static/js/2.e5284820.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e5284820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33df03f2d9b8917199d",
+    "url": "/static/js/main.fca6d8d6.chunk.js"
+  },
+  {
+    "revision": "3a02952197bb0cbd3428",
+    "url": "/static/js/runtime-main.d1a5802d.js"
+  }
+]);
